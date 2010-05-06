6cc4f08f1c385b390edc2d244703811a1102d4fb
DaStar2B <DaStar2B@82a46b14-ece5-f9c8-6756-62631a89e06b>
2010-05-06 17:52:01 +0000

    Deleted tempTrunk folder.

--- tempTrunk/Include/Console.h
+++
@@ -1,8 +0,0 @@
-#ifndef CSE168_CONSOLE_H_INCLUDED
-
-void warning(const char *fmt,...);
-void error(const char *fmt,...);
-void debug(const char *fmt,...);
-void fatal(const char *fmt,...);
-
-#endif // CSE168_CONSOLE_H_INCLUDED

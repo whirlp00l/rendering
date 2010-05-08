5ac5f8370b1533c7b058e6fb7a9173d3dc4b0ab2
DaStar2B <DaStar2B@82a46b14-ece5-f9c8-6756-62631a89e06b>
2010-05-08 23:34:28 +0000

    Created and copied over all provided classes for assignment 3.

---
+++ Include/Assignment3.h
@@ -0,0 +1,13 @@
+#ifndef CSE168_ASSIGNMENT_3_H_INCLUDED
+#define CSE168_ASSIGNMENT_3_H_INCLUDED
+
+class Assignment3
+{
+public:
+	Assignment3();
+	virtual ~Assignment3();
+
+	void makeCornellScene();
+};
+
+#endif // CSE168_ASSIGNMENT_3_H_INCLUDED

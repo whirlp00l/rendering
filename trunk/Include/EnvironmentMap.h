c40b49c8f806ff2c8b0007931d506b42e6466645
DaStar2B <DaStar2B@82a46b14-ece5-f9c8-6756-62631a89e06b>
2010-05-19 16:29:52 +0000

    Added a long explanatory comment to the environment map class.

--- trunk/Include/EnvironmentMap.h
+++ trunk/Include/EnvironmentMap.h
@@ -3,6 +3,15 @@
 
 #include "Vector3.h"
 
+/*
+ * created with help from http://www.debevec.org/Probes/:
+ * If we consider the images to be normalized to have coordinates u=[-1,1], v=[-1,1], 
+ * we have theta=atan2(v,u), phi=pi*sqrt(u*u+v*v). The unit vector pointing in the 
+ * corresponding direction is obtained by rotating (0,0,-1) by phi degrees around the 
+ * y (up) axis and then theta degrees around the -z (forward) axis. If for a direction 
+ * vector in the world (Dx, Dy, Dz), the corresponding (u,v) coordinate in the light 
+ * probe image is (Dx*r,Dy*r) where r=(1/pi)*acos(Dz)/sqrt(Dx^2 + Dy^2).
+ */
 class EnvironmentMap
 {
 public:

a6add24875efbefa974b0eb13ad1220c315d618c
DaStar2B <DaStar2B@82a46b14-ece5-f9c8-6756-62631a89e06b>
2010-06-07 01:24:50 +0000

    Made it possible to specify what percentage of light is refracted in a refracted material (the rest will be reflected).

--- trunk/Include/SpecularRefractor.h
+++ trunk/Include/SpecularRefractor.h
@@ -6,7 +6,8 @@
 class SpecularRefractor : public SpecularReflector
 {
 public:
-	SpecularRefractor( const float & refractiveIndex, const Vector3 & kd = Vector3(1), const float & density = 1.0f );
+	SpecularRefractor( const float & refractiveIndex, const Vector3 & kd = Vector3(1), 
+		               const float & density = 1.0f, const float & percentRefraction = 1.0f );
 	~SpecularRefractor();
 
 	virtual void preCalc() {}
@@ -34,6 +35,7 @@ protected:
 	bool getRefractedRay( Ray& refractedRay, const Ray& ray, const HitInfo& hit, const Scene& scene ) const;
 
 	float m_density; // in g/cm^3
+	float m_percentRefraction; // in range [0,1]; specifies how much light should be refracted vs. reflected
 };
 
 #endif // SPECULAR_REFRACTOR_H
